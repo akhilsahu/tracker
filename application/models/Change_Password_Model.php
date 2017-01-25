37331cc8f65648febd8e302f016aade4cac05c1f
thirdthought1990 <thethirdthought1990@gmail.com>
2017-01-25 11:19:57 +0530

    text

---
+++ application/models/Change_Password_Model.php
@@ -0,0 +1,17 @@
+<?php
+ class Change_Password_Model extends CI_Model{
+
+  public function select_password($old,$id){
+      $query=$this->db->query("SELECT *
+                                 FROM tab_users 
+                                 WHERE txt_password = '$old' AND int_user_id='$id'");
+        return $query->row_array(); 	
+    }
+    public function update_password($new,$int_user_id){
+     $data = array('txt_password' => $new);
+                   $this->db->where('int_user_id',$int_user_id);
+                  // print_r($data);exit;
+		   return $this->db->update("tab_users",$data);   
+    }
+}
+?>
---
+++ application/views/client/assign.php
@@ -0,0 +1,41 @@
+<html>
+    <head>      
+        <title>Device|Member Assignment</title>
+        <script src="<?php echo base_url();?>/assets/js/model.js"></script>
+        <script src="<?php echo base_url();?>/assets/js/model1.js"></script>
+         <link href="<?php echo base_url();?>/assets/css/css.css" rel="stylesheet" type="text/css" />
+    </head>
+    
+    <body>
+
+<div class="container">
+  <h2>Modal Example</h2>
+  <!-- Trigger the modal with a button -->
+  <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Open Modal</button>
+
+  <!-- Modal -->
+  <div class="modal fade" id="myModal" role="dialog">
+    <div class="modal-dialog">
+    
+      <!-- Modal content-->
+      <div class="modal-content">
+        <div class="modal-header">
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+          <h4 class="modal-title">Modal Header</h4>
+        </div>
+        <div class="modal-body">
+          <p>Some text in the modal.</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+        </div>
+      </div>
+      
+    </div>
+  </div>
+  
+</div>
+
+</body>
+
+</html>
